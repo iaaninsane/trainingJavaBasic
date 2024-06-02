60cab3a7719b7e642940dc7e7f5fbec806427ced
iaan <iaaninsane@gmail.com>
2024-06-01 21:34:52 -0300

    tipos primitivos

---
+++ src/fundamentos/TiposPrimitivos.java
@@ -0,0 +1,27 @@
+package fundamentos;
+
+public class TiposPrimitivos {
+
+	public static void main(String[] args) {
+		// Informações do funcionário
+		
+		//Tipos numéricos interios
+		byte anosDeEmpresa=23; //1 byte
+		short numeroDevoos =542; //2 bytes
+		int id = 378_523; //4 bytes
+		long pontosAcumulados = 3_698_521_447L; //8 bytes
+		
+		//Tipos Numéricos Reais
+		float salario = 75_364.32F;
+		double vendasTotal = 785_234_367.6;
+		
+		//Tipo booleano
+		boolean ativo = true;
+		
+		//Tipo caractere
+		char status = 'b';
+		
+		
+	}
+
+}
