cc0149bb04e16d724b3e515d47d602065dc368e3
iaan <iaaninsane@gmail.com>
2024-06-06 00:05:28 -0300

    convertendo string em numeros

---
+++ src/fundamentos/StringToNumber.java
@@ -0,0 +1,17 @@
+package fundamentos;
+
+public class StringToNumber {
+
+	public static void main(String[] args) {
+		
+		String palavra = "3";
+		
+		System.out.println(Integer.parseInt(palavra));
+		System.out.println(Double.parseDouble(palavra));
+		System.out.println(Float.parseFloat(palavra));
+		System.out.println(Long.parseLong(palavra));
+		
+
+	}
+
+}
